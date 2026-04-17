a883aeb2fde85291c3ac23f872430426ee007560
adrienLapasset <contact@cascadestudio.fr>
2026-04-17 10:03:07 +0200

    fix gelica font

--- apps/web/app/fonts.ts
+++ apps/web/app/fonts.ts
@@ -3,42 +3,22 @@ import localFont from "next/font/local";
 export const gelica = localFont({
   src: [
     {
-      path: "./fonts/fonnts.com-Gelica-Extra-Light.otf",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "./fonts/fonnts.com-Gelica-Light.otf",
+      path: "./fonts/Gelica-Light.otf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "./fonts/fonnts.com-Gelica-Regular.otf",
+      path: "./fonts/Gelica-Regular.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/fonnts.com-Gelica-Italic.otf",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "./fonts/fonnts.com-Gelica-Medium.otf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./fonts/fonnts.com-Gelica-Semi-Bold.otf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "./fonts/fonnts.com-Gelica-Bold.otf",
+      path: "./fonts/Gelica-Bold.otf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./fonts/fonnts.com-Gelica-Black.otf",
+      path: "./fonts/Gelica-Black.otf",
       weight: "900",
       style: "normal",
     },
